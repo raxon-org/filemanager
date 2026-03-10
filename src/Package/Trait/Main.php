9ccc06ba217dd4d6b7d75bb641d9230eaa344b12
Remco van der Velde <remco@universeorange.com>
2026-03-10 16:54:39 +0100

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -284,14 +284,16 @@ trait Main {
                     */
 
                     $list = Entity::list($object, $em, $node->role_system(), $options);
-                    ddd($list);
-                    foreach($list as $nr => $item){
-                        if(property_exists($item, 'role')){
-                            foreach($item->role as $role){
-                                d($role->name);
+                    if(array_key_exists('nodeList', $list)){
+                        foreach($list['nodeList'] as $nr => $item){
+                            if(property_exists($item, 'role')){
+                                foreach($item->role as $role){
+                                    d($role->name);
+                                }
                             }
                         }
                     }
+
                 }
         }
         return $list;

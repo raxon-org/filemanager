fe3d766d43259e0c39415845566445eb03327f24
Remco van der Velde <remco@universeorange.com>
2026-04-22 17:33:15 +0200

    x

--- src/Application/Public/Module/File.js
+++ src/Application/Public/Module/File.js
@@ -1398,7 +1398,7 @@ file.download = async (element) => {
     a.click();
     document.body.removeChild(a);
     // Release memory
-    //URL.revokeObjectURL(blobUrl);
+    URL.revokeObjectURL(blobUrl);
 }
 
 file.rename = (element) => {

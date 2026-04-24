2e651a7d5db8a05a997a0fddf825858054554dd8
Remco van der Velde <remco@universeorange.com>
2026-04-24 14:19:24 +0200

    x

--- src/Application/Public/Module/File.js
+++ src/Application/Public/Module/File.js
@@ -1376,7 +1376,7 @@ file.download = async (element) => {
     }
     console.log(user.data());
 
-    const url = route.url + '?url=' + node.url + '&key=' + user.data('user.key');
+    const url = route.url + '?url=' + _('_').htmlspecialchars(node.url) + '&key=' + user.data('key');
     console.log(url);
     console.log('https://api.workandtravel.staging/FileSystem/?url=%2Fmnt%2FVps3%2FMount%2FAudio%2FMusic%2F17-2026%2FRaXon+-+Roadtrip.230.mp3&key=6d783970-a88f-4ff2-a528-25cf47ae6de2-3df28013-1afc-408b-9435-57ee2e330c65');
 

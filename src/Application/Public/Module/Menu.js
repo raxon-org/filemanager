d0ef48dfc78b649a99b213e766e88b09a3ff4bed
Remco van der Velde <remco@universeorange.com>
2026-04-26 16:48:07 +0200

    init

--- src/Application/Public/Module/Menu.js
+++ src/Application/Public/Module/Menu.js
@@ -94,6 +94,7 @@ menu.upload = () => {
                     url: file.data.get('route.backend.upload'),
                     maxFilesize: file.data.get('upload.max.filesize'),
                     chunking: file.data.get('upload.chunking'),
+                    parallelChunkUploads: file.data.get('upload.parallel.chunk.uploads'),
                     // chunkSize: file.data.get('upload.chunk.size'),
                     // parallelUploads: file.data.get('upload.parallel.uploads'),
                     // timeout: file.data.get('upload.timeout'),

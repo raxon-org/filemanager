64422341aca204819710c1f11a15e2219efd20a0
Remco van der Velde <remco@universeorange.com>
2026-09-23 20:39:13 +0200

    init

--- src/Application/Public/Module/File.js
+++ src/Application/Public/Module/File.js
@@ -1340,6 +1340,7 @@ file.exception = {
             if (dialog_active) {
                 dialog_active.removeClass('dialog-active');
             }
+            console.log(data);
             div.addClass('dialog dialog-active dialog-message');
             div.innerHTML = '<div class="head"><h1><img src="/Application/Filemanager/Icon/Icon.png" class="icon"> Message </h1><span class="close"><i class="fas fa-window-close"></i></span><span class="minimize"><i class="far fa-window-minimize"></i></span></div><div class="body"><p class="message">' + data?.message + '</p><form name="message"><button type="submit" name="ok">Ok</button></form></div>';
             // let body = element.closest('.body');

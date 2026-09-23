94f286d5c95d9fb93c45fd6e65447bcca078eed0
Remco van der Velde <remco@universeorange.com>
2026-09-23 15:20:03 +0200

    init

--- src/Application/Public/Module/File.js
+++ src/Application/Public/Module/File.js
@@ -1727,7 +1727,7 @@ file.new_directory = (element) => {
             }
             header("Authorization", 'Bearer ' + token);
             request(route.directory.new, node, (url, data) => {
-                if(data?.class === 'Raxon\\\\Exception\\\\DirectoryNotExistException'){
+                if(data?.class === 'Raxon\\Exception\\DirectoryNotExistException'){
                     if(data?.message){
                         let message = div.select('.message');
                         message.html(data?.message);

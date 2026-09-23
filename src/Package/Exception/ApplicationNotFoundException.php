ff85a1649ac1bfb03865d6dcdf392ac23c088b20
Remco van der Velde <remco@universeorange.com>
2026-09-23 19:31:20 +0200

    init

---
+++ src/Package/Exception/ApplicationNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           14-03-2021
+ * @copyright       (c) Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *  -    all
+ */
+namespace Package\Raxon\Filemanager\Exception;
+
+use Exception;
+
+class ApplicationNotFoundException extends Exception {
+
+}
